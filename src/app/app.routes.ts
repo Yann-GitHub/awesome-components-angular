78b7d4426fcadd1a4d7e1470896d78458a1bd0af
Yann <ybarlet@gmail.com>
2024-12-08 16:21:01 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { SocialMediaComponent } from './social-media/components/social-media/social-media.component';
+import { TestComponent } from './core/components/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   {
+  //     path: 'social-media',
+  //     loadComponent: () =>
+  //       import(
+  //         './social-media/components/social-media/social-media.component'
+  //       ).then((m) => m.SocialMediaComponent),
+  //   },
+  { path: 'social-media', component: SocialMediaComponent },
+  { path: 'test', component: TestComponent },
+  //   { path: '**', redirectTo: 'social-media' },
+];
