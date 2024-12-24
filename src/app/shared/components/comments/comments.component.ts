3489cf7f2e5f2be15d96165d44a120cb3a151fce
Yann <ybarlet@gmail.com>
2024-12-24 15:42:25 +0100

    Enhance comments component with animations and styling adjustments

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -8,6 +8,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule, DatePipe } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import {
+  state,
+  trigger,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-comments',
@@ -22,6 +29,28 @@ import { FormControl } from '@angular/forms';
     MatIconModule,
     ReactiveFormsModule,
   ],
+  animations: [
+    trigger('listItem', [
+      state(
+        'default',
+        style({
+          transform: 'scale(1)',
+          backgroundColor: 'white',
+          zIndex: 1,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+          backgroundColor: 'rgb(201, 157, 242)',
+          zIndex: 2,
+        })
+      ),
+      transition('default => active', [animate('250ms ease-in-out')]),
+      transition('active => default', [animate('250ms ease-in-out')]),
+    ]),
+  ],
 
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
@@ -33,6 +62,8 @@ export class CommentsComponent implements OnInit {
   @Output() newComment = new EventEmitter<string>(); // Output property to emit new comment data to parent component - same as props in React
 
   commentCtrl!: FormControl;
+  animationStates: { [key: number]: 'default' | 'active' } = {};
+  // listItemAnimationState: 'default' | 'active' = 'default';
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -42,6 +73,11 @@ export class CommentsComponent implements OnInit {
       Validators.required,
       Validators.minLength(10),
     ]);
+
+    // Initialize the animation states for each comment
+    for (let index in this.comments) {
+      this.animationStates[index] = 'default';
+    }
   }
 
   onLeaveComment() {
@@ -50,5 +86,13 @@ export class CommentsComponent implements OnInit {
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+  }
+
+  onListItemMouseEnter(index: number) {
+    this.animationStates[index] = 'active';
+  }
+
+  onListItemMouseLeave(index: number) {
+    this.animationStates[index] = 'default';
   }
 }
